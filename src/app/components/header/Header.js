40be292f5e50fdb3854aa7c8ff1903bd9bff71c0
Siddharth0101 <siddsikarwar2001@gmail.com>
2024-06-29 13:10:08 +0530

    Header

--- src/app/components/header/Header.js
+++ src/app/components/header/Header.js
@@ -1,89 +1,179 @@
 "use client";
-import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
-import HeaderDropDown from "./HeaderDropDown";
-import Search from "./Search";
-import Cart from "./Cart";
+import { useState } from "react";
+import { FiMenu, FiArrowRight } from "react-icons/fi";
 
-const Header = () => {
+export default function Header() {
   return (
-    <div className="container mx-auto relative">
-      <div className="fixed inset-x-0 top-0 z-50 flex justify-between items-center">
-        <SlideTabs />
-        <div className="absolute right-20">
-          <HeaderDropDown />
-        </div>
-        <div className="absolute right-80">
-          <Search />
-        </div>
-        <div className="absolute right-60">
-          <Cart />
-        </div>
-      </div>
+    <div className="bg-gray-50">
+      <FlipNav />
+      <div className="h-0" />
+    </div>
+  );
+}
+
+const FlipNav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="bg-white p-4 border-b-[1px] border-gray-200 flex items-center justify-between relative">
+      <NavLeft setIsOpen={setIsOpen} />
+      <NavRight />
+      <NavMenu isOpen={isOpen} />
+    </nav>
+  );
+};
+
+const Logo = () => {
+  // Temp logo from https://logoipsum.com/
+  return (
+    <svg
+      width="50"
+      height="39"
+      viewBox="0 0 50 39"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="fill-gray-800"
+    >
+      <path
+        d="M16.4992 2H37.5808L22.0816 24.9729H1L16.4992 2Z"
+        stopColor="#000000"
+      ></path>
+      <path
+        d="M17.4224 27.102L11.4192 36H33.5008L49 13.0271H32.7024L23.2064 27.102H17.4224Z"
+        stopColor="#000000"
+      ></path>
+    </svg>
+  );
+};
+
+const NavLeft = ({ setIsOpen }) => {
+  return (
+    <div className="flex items-center gap-6">
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        className="block lg:hidden text-gray-950 text-2xl"
+        onClick={() => setIsOpen((pv) => !pv)}
+      >
+        <FiMenu />
+      </motion.button>
+      <Logo />
+      <NavLink text="Solutions" />
+      <NavLink text="Community" />
+      <NavLink text="Pricing" />
+      <NavLink text="Company" />
     </div>
   );
 };
 
-const SlideTabs = () => {
-  const [position, setPosition] = useState({
-    left: 0,
-    width: 0,
+const NavLink = ({ text }) => {
+  return (
+    <a
+      href="#"
+      rel="nofollow"
+      className="hidden lg:block h-[30px] overflow-hidden font-medium"
+    >
+      <motion.div whileHover={{ y: -30 }}>
+        <span className="flex items-center h-[30px] text-gray-500">{text}</span>
+        <span className="flex items-center h-[30px] text-indigo-600">
+          {text}
+        </span>
+      </motion.div>
+    </a>
+  );
+};
+
+const NavRight = () => {
+  return (
+    <div className="flex items-center gap-4">
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        className="px-4 py-2 bg-gradient-to-r from-violet-600 to-indigo-600 bg-clip-text text-transparent font-medium rounded-md whitespace-nowrap"
+      >
+        Sign in
+      </motion.button>
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        className="px-4 py-2 bg-gradient-to-r from-violet-600 to-indigo-600 text-white font-medium rounded-md whitespace-nowrap"
+      >
+        Sign up
+      </motion.button>
+    </div>
+  );
+};
+
+const NavMenu = ({ isOpen }) => {
+  return (
+    <motion.div
+      variants={menuVariants}
+      initial="closed"
+      animate={isOpen ? "open" : "closed"}
+      className="absolute p-4 bg-white shadow-lg left-0 right-0 top-full origin-top flex flex-col gap-4"
+    >
+      <MenuLink text="Solutions" />
+      <MenuLink text="Community" />
+      <MenuLink text="Pricing" />
+      <MenuLink text="Company" />
+    </motion.div>
+  );
+};
+
+const MenuLink = ({ text }) => {
+  return (
+    <motion.a
+      variants={menuLinkVariants}
+      rel="nofollow"
+      href="#"
+      className="h-[30px] overflow-hidden font-medium text-lg flex items-start gap-2"
+    >
+      <motion.span variants={menuLinkArrowVariants}>
+        <FiArrowRight className="h-[30px] text-gray-950" />
+      </motion.span>
+      <motion.div whileHover={{ y: -30 }}>
+        <span className="flex items-center h-[30px] text-gray-500">{text}</span>
+        <span className="flex items-center h-[30px] text-indigo-600">
+          {text}
+        </span>
+      </motion.div>
+    </motion.a>
+  );
+};
+
+const menuVariants = {
+  open: {
+    scaleY: 1,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.1,
+    },
+  },
+  closed: {
+    scaleY: 0,
+    transition: {
+      when: "afterChildren",
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const menuLinkVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+  },
+  closed: {
+    y: -10,
     opacity: 0,
-  });
-
-  return (
-    <ul
-      onMouseLeave={() => {
-        setPosition((pv) => ({
-          ...pv,
-          opacity: 0,
-        }));
-      }}
-      className="relative mx-auto flex w-fit rounded-full border-2 border-black bg-white p-1"
-    >
-      <Tab setPosition={setPosition}>Home</Tab>
-      <Tab setPosition={setPosition}>Store</Tab>
-      <Tab setPosition={setPosition}>About Us</Tab>
-      <Tab setPosition={setPosition}>fun</Tab>
-
-      <Cursor position={position} />
-    </ul>
-  );
+  },
 };
 
-const Tab = ({ children, setPosition }) => {
-  const ref = useRef(null);
-
-  return (
-    <li
-      ref={ref}
-      onMouseEnter={() => {
-        if (!ref?.current) return;
-
-        const { width } = ref.current.getBoundingClientRect();
-
-        setPosition({
-          left: ref.current.offsetLeft,
-          width,
-          opacity: 1,
-        });
-      }}
-      className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base"
-    >
-      {children}
-    </li>
-  );
+const menuLinkArrowVariants = {
+  open: {
+    x: 0,
+  },
+  closed: {
+    x: -4,
+  },
 };
-
-const Cursor = ({ position }) => {
-  return (
-    <motion.li
-      animate={{
-        ...position,
-      }}
-      className="absolute z-0 h-7 rounded-full bg-black md:h-12"
-    />
-  );
-};
-
-export default Header;
